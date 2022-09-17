1d67da557f1817a2b77d7972fb8470fc62b79cfc
Phan Hoang <17520519@gm.uit.edu.vn>
2022-09-17 14:13:07 +0700

    WIP: add list candidate UI

--- src/app/candidate-management/candidate-management.module.ts
+++ src/app/candidate-management/candidate-management.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CandidatesComponent } from './candidates/candidates.component';
-import { Route, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CandidatesComponent} from './candidates/candidates.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
-const routes: Routes = [{ path: '', component: CandidatesComponent }];
+const routes: Routes = [{path: '', component: CandidatesComponent}];
 
 @NgModule({
   declarations: [CandidatesComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatButtonModule, MatIconModule],
 })
-export class CandidateManagementModule {}
+export class CandidateManagementModule {
+}
--- src/app/candidate-management/candidates/candidates.component.ts
+++ src/app/candidate-management/candidates/candidates.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-candidates',
   templateUrl: './candidates.component.html',
   styleUrls: ['./candidates.component.scss']
 })
-export class CandidatesComponent implements OnInit {
-
-  constructor() { }
+export class CandidatesComponent implements OnInit, AfterViewInit {
+  @ViewChild('selectAll', {read: ElementRef}) checkbox: ElementRef<HTMLInputElement> | undefined;
+  isSelectedAll: boolean = false;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+  }
+
+  toggleSelected() {
+    this.isSelectedAll = !this.isSelectedAll;
+  }
 }
